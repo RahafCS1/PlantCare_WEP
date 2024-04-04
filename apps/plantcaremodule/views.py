0aa7a7b42005555864020862b910332625460191
= <=>
2024-04-04 03:33:25 +0300

    modifications

--- apps/plantcaremodule/views.py
+++ apps/plantcaremodule/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.template.loader import get_template
 def index(request):
     #study the request
     # render the appropriate template for this request
